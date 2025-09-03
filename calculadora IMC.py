c8b30af5c51e5a3cb92c87cd15ae748c2cd2d53f
Ashzin <143672778+xaredex@users.noreply.github.com>
2025-09-03 16:29:00 -0300

    Update calculadora IMC.py

--- calculadora IMC.py
+++ calculadora IMC.py
@@ -6,3 +6,15 @@ imc = round(imc, 2)
 
 print('Seu IMC é: ' + str(imc))
 
+if imc < 18.5:
+    print('Voce está abaixo do peso normal!')
+elif imc < 24.9:
+    print('Voce está no peso normal!')
+elif imc < 29.9:
+    print('Voce está com excesso de peso!')
+elif imc < 34.9:
+    print('Voce está com obesidade classe I')
+elif imc < 39.9:
+    print('Voce está com obesidade classe II')
+else:
+    print('Voce está com obesidade classe III')
