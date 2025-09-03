8c23d9b73d85d2f38ece1283d8f260e76a387b67
Ashzin <143672778+xaredex@users.noreply.github.com>
2025-09-03 14:33:07 -0300

    Add files via upload

---
+++ calculadora IMC.py
@@ -0,0 +1,7 @@
+peso = int(input('Digite o seu peso em kg: '))
+altura = float(input('Digite a sua altura em metros: '))
+
+imc = peso / (altura ** 2)
+imc = round(imc)
+
+print('Seu IMC é: ' + str(imc))
